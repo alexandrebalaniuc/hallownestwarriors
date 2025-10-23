9b4f11b6c732e2b7d15ae6647a7b7b2159b5484b
Alexandre <alexandrebalaniucm@gmail.com>
2025-10-23 15:09:59 -0700

    update characters

--- characters/characters.py
+++ characters/characters.py
@@ -1,6 +1,8 @@
 Knight
 Hornet
 Myla
+Quirrel
+Cloth
 Lost Kin
 Hollow Knight
 The False Knight
@@ -8,4 +10,43 @@ The Radiance
 Mantis Lords
 Grimm
 Nightmare Grim
-
+The Watcher Knight
+Broken Vessel
+Herrah the Beast
+Lurien the Watcher
+Monomon the teacher
+Watcher Knights
+Uumu
+Vengefly king
+Soul warrior
+Traitor Lord
+Soul Master
+Sheo
+Nosk
+Hive Knight
+Gruz mother
+Nailsage Sly
+God Tamer
+Dung Defender
+Crystal Guardian 
+Flukemarm
+Oro & Mato 
+The Collector
+Brooding mawlek
+Mighty Zote
+Elder Hu
+Galien 
+Gord
+Markoth
+Marmu
+No-Eyes
+Xero
+Pure Vessel
+Failed Champion
+Winged Nosk
+White Defender
+Soul Tyrant
+Sisters of Battle 
+Enraged Guardian
+The Hunter
+Tiso
